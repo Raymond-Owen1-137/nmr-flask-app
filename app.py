8a21c0384ba715aac258174c2b93c7003f1c8734
Raymond-Owen1-137 <ow042599@ucf.edu>
2025-02-24 17:27:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -121,6 +121,6 @@ def predict():
 # -----------------------------------------------------
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides a PORT env variable
+    port = int(os.environ.get("PORT", 10000))  # Render provides a PORT env variable
     app.run(host="0.0.0.0", port=port, debug=True)
 
